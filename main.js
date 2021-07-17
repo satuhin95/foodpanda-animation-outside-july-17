120028caf37a0fcb83cd938f0b4b537e6d164dd7
MD. SAIFUDDIN <57839548+satuhin95@users.noreply.github.com>
2021-07-17 16:16:52 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,36 @@
-    
-    setTimeout(headline, 1500);
-    function headline () {
-        if ($( ".slide_1 .element_2 .headlinebeshi" ).hasClass( "showZoomIn" )  ) {
-            $( ".slide_1 .element_2 .headlinebeshi" ).removeClass( "showZoomIn" ).addClass( "hideZoomOut" );
-            $( ".slide_1 .element_3 .minutes" ).removeClass( "showZoomIn" ).addClass( "hideZoomOut" );
-            setTimeout(function() {
-                $( ".slide_1 .element_2 .headline" ).removeClass( "hideZoomOut" ).addClass( "showZoomIn" );
-                $( ".slide_1 .element_3 .delivery" ).removeClass( "hideZoomOut" ).addClass( "showZoomIn" );
-                setTimeout(headlinebeshi, 1500);
-            }, 500);
-        }else{
-            $(".slide_1 .element_2 .headline").addClass("hideZoomOut");
-            $(".slide_1 .element_3 .delivery").addClass("hideZoomOut");
-            setTimeout(headlinebeshi, 500);
-        }                               
-    }
+  setTimeout( slide_1, 1500 );
+        function slide_1 ( ) {
+            $(".slide_1 .element_2").addClass('hideZoomOut');
+            $(".slide_1 .element_2 img").removeClass('showZoomIn');
+            $(".slide_1 .element_3").removeClass('hidden');
+            $(".slide_1 .element_3 img").addClass('showZoomIn');
 
-    function headlinebeshi () {
-        if( $(".slide_1 .element_2 .headlinebeshi").hasClass("hideZoomOut")  ){
-            $( ".slide_1 .element_2 .headline" ).removeClass( "showZoomIn" ).addClass( "hideZoomOut" );
-            $( ".slide_1 .element_3 .delivery" ).removeClass( "showZoomIn" ).addClass( "hideZoomOut" );
-            setTimeout(function() {
-                $(".slide_1 .element_2 .headlinebeshi").removeClass("hideZoomOut").addClass("showZoomIn");
-                $(".slide_1 .element_3 .minutes").removeClass("hideZoomOut").addClass("showZoomIn");
-                setTimeout(outside, 1500);
-            }, 500);
-        }else{
-            $(".slide_1 .element_2 .headlinebeshi").removeClass("hidden").addClass("showZoomIn");
-            $(".slide_1 .element_3 .minutes").removeClass("hidden").addClass("showZoomIn");
-            setTimeout(headline, 1500);
-        }
-    }
-
-    function outside(){
-        if( $(".slide_1 .element_2 .headlinebeshi").hasClass("showZoomIn")  ){
-            $( ".slide_1" ).addClass( "hideZoomOut" );
-           
-            setTimeout(function() {
-                $(".slide_2").removeClass("hidden");
-                $(".slide_2 .element_2 ").addClass("showZoomIn");
-                $(".slide_2 .element_3 ").addClass("showZoomIn");
-                $(".slide_2 .element_4 ").addClass("showZoomIn");
-                $(".slide_2 .element_5 ").addClass("showZoomIn");
-                setTimeout(headline, 1500);
-            }, 500);
-        }else{
-            $( ".slide_1" ).removeClass( "hideZoomOut" ).addClass("showZoomIn");
-            setTimeout(headlinebeshi, 1500);
+            setTimeout( slide_2, 2000 );
         }
 
-    }
+        function slide_2 ( ) {
+            $(".slide_1 .element_1").css('height','140');
+            $(".slide_1 .element_3 .minutes").css('height','81px').css('top','50px');
+            $(".slide_1 .element_4").addClass('hideZoomOut').removeClass('showZoomIn');
+            $(".slide_1 .element_5").removeClass('hidden').removeClass('hideZoomOut').addClass('showZoomIn');
+            $('.slide_1 .element_6').css({'top':'218px','left':'98px','width':'103px','height':'29px'});
+            setTimeout( slide_3, 2000 );
+        }
 
+        function slide_3 ( ) {
+            $(".slide_1 .element_4").removeClass('hideZoomOut').addClass('showZoomIn');
+            $(".slide_1 .element_5").removeClass('showZoomIn').addClass('hideZoomOut');
+
+            $(".slide_1 .element_1").css('height', '174px');
+            $(".slide_1 .element_3 .minutes").css('height','105px').css('top','59px');
+            $('.slide_1 .element_6').css({'top':'197px','left':'89px','width':'117px'});
+
+            setTimeout(function(){
+                $(".slide_1 .element_2").removeClass('hideZoomOut');
+                $(".slide_1 .element_2 img").addClass('showZoomIn');
+                $(".slide_1 .element_3").addClass('hidden');
+                $(".slide_1 .element_3 img").removeClass('showZoomIn');
+
+                setTimeout( slide_1, 2000 );
+            }, 1500);
+        }
